99199096f1fc5d08bc6c34557f047679dbedaa6e
thinh <thinhqn999@gmail.com>
2024-09-26 21:23:45 +0700

    final admin page

--- NhaSachOnlineTrangAdmin/Controllers/BookController.cs
+++ NhaSachOnlineTrangAdmin/Controllers/BookController.cs
@@ -53,5 +53,43 @@ namespace NhaSachOnlineTrangAdmin.Controllers
       TempData["msg"] = "Đã xảy ra lỗi. Vui lòng kiểm tra lại.";
       return View(model);
     }
+
+    public IActionResult GetAll()
+    {
+      var data = bookService.GetAll();
+      return View(data);
+    }
+
+    public IActionResult Update(int id)
+    {
+      var model = bookService.FindById(id);
+      model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorID }).ToList();
+      model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GenreId }).ToList();
+      model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Book model)
+    {
+      model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorID }).ToList();
+      model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GenreId }).ToList();
+      model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      var result = bookService.Update(model);
+      if (result)
+      {
+        return RedirectToAction("GetAll");
+      }
+      TempData["msg"] = "Lỗi phía server";
+
+      return View(model);
+    }
+
+    public IActionResult Delete(int id) => RedirectToAction("GetAll", new { result = bookService.Delete(id) });
   }
 }
